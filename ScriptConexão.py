2fcaced58226811f61ea627d1282a7acbf866eb3
Hendryl <75086675+HendrylXamann@users.noreply.github.com>
2025-03-29 21:27:11 -0300

    Update ScriptConexão.py

--- ScriptConexão.py
+++ ScriptConexão.py
@@ -1,7 +1,6 @@
 import psycopg2
 import sys
 
-# Função que faz a conexão c/banco (postgresql)
 def conectar():
     connection = psycopg2.connect(
         host="localhost",
@@ -11,13 +10,11 @@ def conectar():
     )
     return connection
 
-# Função que passa os dados, como parametros, para a função de inserção
 def transportadora(nome, valor, validade):
     connection = conectar()
     inserir(connection, nome, valor, validade)
     connection.close()
 
-# Função que coloca os dados recebidos na tabela do bd
 def inserir(connection, nome, valor, validade):
     cursor = connection.cursor()
     cursor.execute(
@@ -29,7 +26,6 @@ def inserir(connection, nome, valor, validade):
     )
     connection.commit()
 
-# Script (explicação do código mais abaixo)
 if __name__ == "__main__": 
     if len(sys.argv) != 4:
         print("Uso: python script.py <nome> <valor> <data>")
@@ -40,18 +36,3 @@ if __name__ == "__main__":
     data = sys.argv[3]
 
     transportadora(nome, valor, data)
-    
-#Explicando script:
-"""
-if __name__ == "__main__": # Validação p/ ver se o script está sendo executado como principal 
-    if len(sys.argv) != 4: #Vê se foi inserido todos os parâmetros necessários
-        print("Uso: python script.py <nome> <valor> <data>")
-        sys.exit(1)
-
-    #Atribui os valores da lista (com os argumentos passados qnd o script for chamado), com index, as respectivas variáveis
-    nome = sys.argv[1]
-    valor = sys.argv[2]
-    data = sys.argv[3]
-
-    transportadora(nome, valor, data) # Chama a função passando as variáveis como parâmetro 
-"""
